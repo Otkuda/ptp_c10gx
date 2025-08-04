abe25fc0ec719e13e144eae852bc178d945d1141
Kirill Kotov <kirill.kotov@spbpu.com>
2025-08-04 14:27:06 +0300

    better rtc test

--- sim/rtc_tb/rtc_tb.py
+++ sim/rtc_tb/rtc_tb.py
@@ -31,6 +31,7 @@ class TB():
     await Timer(CLK_PER * 1, 'ns')
     self.dut.period_ld.value = 0
     self.dut.period_in.value = 0
+    await Timer(CLK_PER*3, 'ns')
 
   async def load_offset(self, offset):
     self.dut.offset_nsec.value = offset << 8
@@ -38,13 +39,39 @@ class TB():
     await Timer(CLK_PER * 1, 'ns')
     self.dut.offset_ld.value = 0
     self.dut.offset_nsec.value = 0
+    await Timer(CLK_PER * 1, 'ns')
+
+  async def set_time(self, sec, nsec):
+    self.dut.time_reg_ns_in.value = nsec
+    self.dut.time_reg_sec_in.value = sec
+    self.dut.time_ld.value = 1
+    await Timer(CLK_PER, 'ns')
+    self.dut.time_ld.value = 0
+    self.dut.time_reg_ns_in.value = 0
+    self.dut.time_reg_sec_in.value = 0
+    
 
 @cocotb.test()
-async def test_parser(dut):
+async def basic_test(dut):
   tb = TB(dut)
   await tb.init()
   await Timer(CLK_PER, 'ns')
   await tb.load_period()
-  await Timer(1000, 'ns')
+  await Timer(CLK_PER * 125, 'ns')
+  assert int(tb.dut.time_ptp_ns.value) == CLK_PER * 125
+  time_b = int(tb.dut.time_ptp_ns.value)
   await tb.load_offset(CLK_PER * 100)
+  assert time_b + CLK_PER * (100 + 2) == int(tb.dut.time_ptp_ns.value)
+  await Timer(100, 'ns')
+
+@cocotb.test()
+async def sec_test(dut):
+  tb = TB(dut)
+  await tb.init()
+  await Timer(CLK_PER, 'ns')
+  await tb.load_period()
+  await Timer(CLK_PER*10, 'ns')
+  await tb.set_time(0, (999950000<<8))
+  await tb.load_offset(CLK_PER*100)
+  await RisingEdge(tb.dut.time_one_pps)
   await Timer(100, 'ns')
